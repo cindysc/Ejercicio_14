8d387834eaadac228a13501ffb46bacf47028565
Cindy <cindy.sanhueza2011@gmail.com>
2018-11-22 07:54:40 -0300

    ejercicio primera parte

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(Document).ready(function(){
+    $('#myform').on('submit', function(event){
+    var text = $('#mytext').val();
+    event.preventDefault();
+    $('#board').append(
+        '<div class="note">'
+        +'<div class="pin">'
+        +'</div>'
+        + text
+        +'</div>')
+        $('#mytext').val('')
+        $('#mytext').focus();
+    });
+    $('#board').on('click', '.pin', function(event){
+        event.stopPropagation();
+        $(this).parent().fadeOut(500);
+    });
+    $('#board').on('click', '.note', function(){
+        $(this).toggleClass('strike');
+    })
+
+})
